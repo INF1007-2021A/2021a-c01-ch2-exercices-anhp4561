d8ede1510e8658177636db3a50c0a848ab5ae884
anhp4561 <anh-tuan.pham@polymtl.ca>
2021-09-09 14:51:15 -0400

    fais exercice.py

--- exercice.py
+++ exercice.py
@@ -2,7 +2,17 @@
 # -*- coding: utf-8 -*-
 def majuscule(mot):
     # TODO completer la fonction ici
-    return mot
+    nouveau_mot = ""
+    for i in range (len(mot)):
+        min = ord(mot[i])
+        lettre_min = chr(min)
+        maj = min - 32
+        lettre_maj = chr (maj)
+        #print (lettre_min)
+        #print (lettre_maj)
+        #upper_case = mot.replace(lettre_min,lettre_maj)
+        nouveau_mot +=lettre_maj
+    return nouveau_mot
 
 
 if __name__ == '__main__':
